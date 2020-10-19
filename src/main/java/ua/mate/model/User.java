5e3a48bc93e745fd57f49bbc7894519df817b0a3
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-10-19 15:44:24 +0300

    Added constructor for User

--- src/main/java/ua/mate/model/User.java
+++ src/main/java/ua/mate/model/User.java
@@ -18,4 +18,9 @@ public class User {
     private Long id;
     private String login;
     private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
 }
